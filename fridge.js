e1b3b8ba8e31c75737c4f77d46676593d5ba1ba6
jasmine <147780973+frailuie@users.noreply.github.com>
2023-12-25 21:09:07 -0500

    Add files via upload

---
+++ fridge.js
@@ -0,0 +1,34 @@
+// making divs button-like
+function openPantry(){
+    document.querySelector(".info-box").style.display = "block";
+}
+
+function closebtn(){
+    document.querySelector(".info-box").style.display = "none";
+
+}
+
+
+function openFreezer(){
+    document.querySelector(".info-box2").style.display = "block";
+}
+
+function closebtn2(){
+    document.querySelector(".info-box2").style.display = "none";
+
+}
+
+function openFridge(){
+    document.querySelector(".info-box3").style.display = "block";
+}
+
+function closebtn3(){
+    document.querySelector(".info-box3").style.display = "none";
+
+}
+
+
+
+
+
+
